30ca0da4518d2041f3325d1ee86772a7021de3c8
Elvisortizumg <101027729+Elvisortizumg@users.noreply.github.com>
2022-05-01 13:05:37 -0600

    Create  Examen-codigo1-serie-3.cpp

---
+++ Examen-codigo1-serie-3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace:: std;
+//.- CONSTRUIR UN PROGRAMA QUE DESPLIEGUE LOS NÚMEROS DEL 20 AL 30.
+int main(int argc, char** argv) {
+	
+	cout<<"numeros del 20 al 30"<<endl;
+	
+	for(int n=20;n<=30;n++)
+	cout<<n<<endl;
+          
+	return 0;
+}
