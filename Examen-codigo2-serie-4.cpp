97cd67ff4718bb3e91ce0588064770e186e9df4a
Elvisortizumg <101027729+Elvisortizumg@users.noreply.github.com>
2022-05-01 15:32:13 -0600

    Create Examen-codigo2-serie-4.cpp

---
+++ Examen-codigo2-serie-4.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include <conio.h>
+//17.- DESPLEGAR ENTEROS ENTRE 50 Y 80
+
+using namespace std;
+int main(){
+	cout<<" Numeros enteros entre 50 a 80"<<endl;
+
+int cont=50;
+while(cont<=80){
+cout<<cont<<""<<endl;
+cont++;
+}
+}
