f404f66d27dfc4f3c7012be6541aaf33d140fa20
Elvisortizumg <101027729+Elvisortizumg@users.noreply.github.com>
2022-05-01 12:14:02 -0600

    Create  Examen-codigo2-serie-1.cpp

---
+++ Examen-codigo2-serie-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int N1;
+	cout<<"Ingrese un numero "<<endl;
+	cin>>N1;
+	if (N1 % 4==0){	
+			cout<<"EL numero si es multiplo de 4 "<<endl;
+	}else {
+		cout<<"EL numero no es multiplo de 4 "<<endl;
+	}	
+	system("pause");
+	return 0;
+}
